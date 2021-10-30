486570ba0b7aaf79e3e85ba2946f1f52a345911b
Jamie <jamie@lambertstock.com>
2021-10-30 14:15:37 +0000

    Fixed order issue

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// DB connect
+mongoose.connect(process.env.MONGO_URI, {
+  dbName: 'WolfTeam',
+}).catch((err) => console.error(err));
+
 // All Passowrd related bits
 require('./modules/init/passport').initialisePassport();
 
@@ -10,8 +15,3 @@ require('./modules/init/express');
 
 // All Discord related bits
 require('./modules/init/discord');
-
-// DB connect
-mongoose.connect(process.env.MONGO_URI, {
-  dbName: 'WolfTeam',
-}).catch((err) => console.error(err));
