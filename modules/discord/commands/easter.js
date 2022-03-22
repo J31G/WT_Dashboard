69af77439d3636c40ee3b1504851ffa841c7ab4c
Jamie Lambertstock <jamie@lambertstock.com>
2022-03-22 21:17:58 +0000

    Changed spacing

--- modules/discord/commands/easter.js
+++ modules/discord/commands/easter.js
@@ -19,15 +19,15 @@ module.exports = {
       .setColor('#00000')
       .addField(
         '🏴󠁧󠁢󠁥󠁮󠁧󠁿 Easter Event - **Every bunny was kung fu fighting!**',
-        'Press the **Daily Event** button to get your daily quest!\n\n_This event will end 18.04.2022 - 12:00 Berlin Time._',
+        '\nPress the **Daily Event** button to get your daily quest!\n\n_This event will end 18.04.2022 - 12:00 Berlin Time._\n',
       )
       .addField(
         '🇩🇪 Osterereignis - **Jeder Hase hat Kung-Fu gekämpft!**',
-        'Drücke die Schaltfläche **Tägliches Ereignis**, um deine tägliche Aufgabe zu erhalten!\n\n_Diese Veranstaltung endet am 18.04.2022 - 12:00 Uhr Berliner Zeit._',
+        '\nDrücke die Schaltfläche **Tägliches Ereignis**, um deine tägliche Aufgabe zu erhalten!\n\n_Diese Veranstaltung endet am 18.04.2022 - 12:00 Uhr Berliner Zeit._\n',
       )
       .addField(
         "🇫🇷 Evénement de Pâques - **Tous les lapins se sont battus en kung-fu!**",
-        'Appuyez sur le bouton **Événement quotidien** pour obtenir votre défi quotidien!, um deine tägliche Aufgabe zu erhalten!\n\n_Cet événement se terminera le 18.04.2022 - 12:00 heure de Berlin._',
+        '\nAppuyez sur le bouton **Événement quotidien** pour obtenir votre défi quotidien!, um deine tägliche Aufgabe zu erhalten!\n\n_Cet événement se terminera le 18.04.2022 - 12:00 heure de Berlin._\n',
       )
       .setImage(
         'https://media.discordapp.net/attachments/900305371787304981/900306192159637594/orange-halloween-banner-with-pumpkin-spider-bats_1017-21309.jpg',
